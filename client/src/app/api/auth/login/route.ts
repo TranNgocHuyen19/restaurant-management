3ef3f48f67b143f082e0b9e5636784dbe4c7d22e
TranNgocHuyen19 <tranngochuyenn1909@gmail.com>
2025-10-15 18:34:05 +0700

    feat: add login route handler

--- client/src/app/api/auth/login/route.ts
+++ client/src/app/api/auth/login/route.ts
@@ -1,7 +1,50 @@
+import authApiRequest from "@/app/apiRequests/auth";
 import { LoginBodyType } from "@/schemaValidations/auth.schema";
 import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { HttpError } from "@/lib/http";
 
 export async function POST(request: Request) {
   const res = (await request.json()) as LoginBodyType;
   const cookieStore = await cookies();
+  try {
+    const { payload } = await authApiRequest.sLogin(res);
+    const { accessToken, refreshToken } = payload.data;
+    const decodedAccessToken = jwt.decode(accessToken) as { exp: number };
+    const decodedRefreshToken = jwt.decode(refreshToken) as { exp: number };
+    cookieStore.set({
+      name: "accessToken",
+      value: accessToken,
+      sameSite: "lax",
+      httpOnly: true,
+      secure: true,
+      path: "/",
+      expires: decodedAccessToken.exp * 1000,
+    });
+    cookieStore.set({
+      name: "refreshToken",
+      value: refreshToken,
+      sameSite: "lax",
+      httpOnly: true,
+      secure: true,
+      path: "/",
+      expires: decodedRefreshToken.exp * 1000,
+    });
+    return Response.json(payload);
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return Response.json(error.payload, {
+        status: error.status,
+      });
+    } else {
+      Response.json(
+        {
+          message: "Có lỗi xảy ra",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
 }
