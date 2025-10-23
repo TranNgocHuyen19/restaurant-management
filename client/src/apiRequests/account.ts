a7cc7c23a3026a621f148a532639a2f1a3ec5345
TranNgocHuyen19 <tranngochuyenn1909@gmail.com>
2025-10-23 17:20:10 +0700

    feat: change password

--- client/src/apiRequests/account.ts
+++ client/src/apiRequests/account.ts
@@ -1,13 +1,18 @@
 import http from "@/lib/http";
 import {
   AccountResType,
+  ChangePasswordBodyType,
   UpdateMeBodyType,
 } from "@/schemaValidations/account.schema";
 
 const PREFIX = "/accounts";
 const accountApiRequests = {
   me: () => http.get<AccountResType>(`${PREFIX}/me`),
+
   updateMe: (body: UpdateMeBodyType) =>
     http.put<AccountResType>(`${PREFIX}/me`, body),
+
+  changePassword: (body: ChangePasswordBodyType) =>
+    http.put<AccountResType>(`${PREFIX}/change-password`, body),
 };
 export default accountApiRequests;
